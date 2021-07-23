3b6b466f137e39c082184434ac30da13746f4c1f
Adrian Leal <lea19019@byui.edu>
2021-07-22 20:22:21 -0500

    delete

--- routes/routesProject/controllers/admin.js
+++ routes/routesProject/controllers/admin.js
@@ -175,35 +175,35 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.findByIdAndRemove(prodId)
-//     .then(() => {
-//       console.log('DESTROYED PRODUCT');
-//       res.redirect('/project/admin/products');
-//     })
-//     .catch(err => {
-//       const error = new Error(err);
-//       error.httStatusCode = 500;
-//       return next(error);
-//     });
-// };
-
-exports.deleteProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product => {
-      if (!product) {
-        return next(new Error('Product not found.'));
-      }
-      fileHelper.deleteFile(product.imageUrl);
-      return Product.deleteOne({ _id: prodId, userId: req.user._id });
-    })
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
-      res.status(200).json({ message: 'Success!' });
+      res.redirect('/project/admin/products');
     })
     .catch(err => {
-      res.status(500).json({ message: 'Deleting product failed.' });
+      const error = new Error(err);
+      error.httStatusCode = 500;
+      return next(error);
     });
 };
+
+// exports.deleteProduct = (req, res, next) => {
+//   const prodId = req.params.productId;
+//   Product.findById(prodId)
+//     .then(product => {
+//       if (!product) {
+//         return next(new Error('Product not found.'));
+//       }
+//       fileHelper.deleteFile(product.imageUrl);
+//       return Product.deleteOne({ _id: prodId, userId: req.user._id });
+//     })
+//     .then(() => {
+//       console.log('DESTROYED PRODUCT');
+//       res.status(200).json({ message: 'Success!' });
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Deleting product failed.' });
+//     });
+// };
